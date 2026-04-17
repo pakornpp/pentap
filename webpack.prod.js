1a75760e0ed7fb21a86777633a06cf7dadffd9ce
Pakorn Poksawat <pakorn.poksawat@gmail.com>
2026-04-17 15:14:18 +0700

    fix: asset path issues

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,9 @@
  import { merge } from 'webpack-merge';
- import common from './webpack.common.js';
+import common from './webpack.common.js';
 
- export default merge(common, {
-   mode: 'production',
- });
+export default merge(common, {
+  mode: 'production',
+  output: {
+    publicPath: './',
+  },
+});
