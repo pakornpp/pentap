446d3622b0abc90ca876a9c44b56704b0ba684c8
Pakorn Poksawat <pakorn.poksawat@gmail.com>
2026-05-12 16:29:15 +0700

    feat: added CNAME to src and auto-copy to dist on deploy

--- webpack.common.js
+++ webpack.common.js
@@ -219,6 +219,7 @@ export default {
       patterns: [
         { from: "./src/sitemap.xml", to: "sitemap.xml" },
         { from: "./src/robots.txt",  to: "robots.txt"  },
+        { from: "./src/CNAME",       to: "CNAME"       },
       ],
     }),
   ],
